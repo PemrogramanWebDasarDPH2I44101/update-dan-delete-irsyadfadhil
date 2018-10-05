e9074bf0957d57a4acc0b91a16e9453280fd030e
Chad <42725274+irsyadfadhil@users.noreply.github.com>
2018-10-05 21:37:51 +0700

    Update list.php

--- list.php
+++ list.php
@@ -1,30 +1,39 @@
 <?php
-require_once("db.php");git 
+require_once("db.php");
 ?>
-<table border=1>
-    <thead>
-        <th>Nama</th>
-        <th>Nim</th>
-        <th>Tanggal Lahir</th>
-    </thead>
-    <tbody>
-<?php
-$sql    = "SELECT * FROM siswa";
-$result = mysqli_query($conn, $sql);
 
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "<tr>";
-        echo "<td>" . $row["nama"]. "</td>"; 
-        echo "<td>" . $row["nim"]. "</td>";
-        echo "<td>" . $row["tgl_lahir"]. "</td>";
-        echo "</tr>";
-    }
-} else {
-    echo "0 results";
-}
-mysqli_close($conn);
-?> 
-    </tbody>
+<table border="1" style="text-align: center;">
+	<thead>
+		<td>Nama</td>
+		<td>NIM</td>
+		<td>Tanggal</td>
+		<td colspan="2"">Edit</td>
+	</thead>
+	<tbody>
+
+		<?php
+		$query = "SELECT * FROM siswa";
+		$result = mysqli_query($conn, $query);
+		$list = mysqli_num_rows($result);
+
+		if ( $list > 0) {
+			while ($row = mysqli_fetch_assoc($result)) {
+				?>
+
+				<tr>
+				<td><?php echo $row['nama']?></td>
+				<td><?php echo $row['nim']?></td>
+				<td><?php echo $row['tgl_lahir']?></td>
+				<td><a href="delete.php?id=<?php echo $row['id']?>">Hapus</a></td>
+				<td><a href="formupdate.php?id=<?php echo $row['id']?>">Update</a></td>
+				</tr>
+				
+				<?php
+			}
+		}else {
+			echo "0 Result";
+		}
+		mysqli_close($conn);
+		?>
+	</tbody>
 </table>
