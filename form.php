e119e6f70f88d8880a3dc429651df0eb77c4cb49
Chad <42725274+irsyadfadhil@users.noreply.github.com>
2018-10-05 21:37:13 +0700

    Update form.php

--- form.php
+++ form.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
-    <head>
-        <title></title>
-    </head>
-    <body>
-        <form action="submit.php" method="post">
-            Nama<input type="text" name="nama" id=""><br>
-            Nim<input type="text" name="nim" id=""><br>
-            Tanggal Lahir<input type="date" name="tgl_lahir" id=""><br>
-            <input type="submit" value="Kirim">
-        </form>
-    </body>
+<html>
+<head>
+	<title>Form</title>
+</head>
+<body>
+	<center><h1>Inputkan Form Data</h1></center>
+	<form action="submit.php" method="POST">
+		Nama : <input type="text" name="nama"><br><br>
+		NIM	 : <input type="text" name="nim"><br><br>
+		Tanggal lahir : <input type="date" name="tgl_lahir"><br><br>
+		<input type="submit" name="submit">
+	</form>
+</body>
 </html>
